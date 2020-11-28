6c3c0548da8cbe47d4803308dd6954c896c51fda
hector <h.pascual@ondho.com>
2020-11-28 23:19:11 +0100

    Añadir imagen y estilos al heading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,54 @@
-import React, { Fragment } from "react";
+import React from "react";
+import styled from '@emotion/styled';
+import imagen from './cryptomonedas.png';
+
+const Contenedor = styled.div`
+	max-width: 900px;
+	margin: 0 auto;
+
+	@media (max-width: 992px) {
+		display: grid;
+		grid-template-columns: repeat(2, 1fr);
+		column-gap: 2rem;
+	}
+`;
+
+const Imagen = styled.img`
+	max-width: 100%;
+	margin-top: 5rem;
+`;
+
+const Heading = styled.h1`
+	font-family: 'Bebas Neue', cursive;
+	color: #ffffff;
+	text-align: left;
+	font-weight: 700;
+	font-size: 50px;
+	margin-bottom: 50px;
+	margin-top: 80px;
+
+	&::after {
+		content: '';
+		width: 100px;
+		height: 6px;
+		display: block;
+		background-color: #66A2FE;
+	}
+`;
 
 function App() {
 	return (
-		<Fragment>
-			<h1>ract</h1>
-		</Fragment>
+		<Contenedor>
+			<div>
+				<Imagen 
+				src={imagen} 
+				alt="imagen cripto"
+				/>
+			</div>
+			<Heading>
+				Cotiza Criptomendas al instante
+			</Heading>
+		</Contenedor>
 	);
 }
 
